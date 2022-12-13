e58d06fde63ab7b32f043a67706d39de2192c185
leehyunseok <lhs01071526524@gmail.com>
2022-12-13 17:59:03 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springwebflux/book/HelloController.kt
@@ -0,0 +1,4 @@
+package com.example.springwebflux.book
+
+class HelloController {
+}
---
+++ src/main/kotlin/com/example/springwebflux/fn/Router.kt
@@ -0,0 +1,28 @@
+package com.example.springwebflux.fn
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class Router {
+
+    @Bean
+    fun helloRouter(handler: HelloHandler) : RouterFunction<ServerResponse> =
+        route()
+            .GET("/hello", handler::sayHello)
+            .build()
+
+
+    @Bean
+    fun userRouter (handler: UserHandler) : RouterFunction<ServerResponse> =
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
+}
